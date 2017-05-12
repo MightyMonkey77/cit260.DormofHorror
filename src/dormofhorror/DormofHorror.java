f669bb2c2528f034f3adeec96250e4ced4da6784
Office Payne <Office Payne@OfficePayne-PC>
2017-05-11 17:35:57 -0700

    5/9/2017 - initial player class

--- src/dormofhorror/DormofHorror.java
+++ src/dormofhorror/DormofHorror.java
@@ -8,6 +8,7 @@ package dormofhorror;
 import cit260.dormofhorror.characters.Player;
 import cit260.dormofhorror.actingjob.Actor;
 import cit260.dormofhorror.mathamatics.Math;
+
 /**
  *
  * @author Office Payne
@@ -18,32 +19,36 @@ public class DormofHorror {
     public static void main(String[] args) {
        Player playerOne = new Player();
        Actor actorOne = new Actor();
-       Math equationOne = new Math();  
-             
+       Math equationsOne = new Math();
+       
+      
+       
        playerOne.setName("Fred Flinstone");
        actorOne.setName ("George Binazowski");
        actorOne.setDescription("You have been invited to spend an evening at a party"
-               + "hosted by your friend Jack Clawsen, at his Dorm room. You eagerly accept"
-               + "his invitation. You have not seen him since you were in grade school together."
-               + "You make the hour long drive using the sirection provided to arrive at what "
-               + "appears to be a rundown set of dorms. You think, 'meh! Stayed in worse' and walk "
-               + "into and up to his room. You knock and are greated by Jack with a hearty hello "
-               + "and a handshake. He asks how you have been and reminisces about the past. He  "
-               + "teases you about how much of a scardy cat you were. After an hour passes he "
-               + "excuses himself and says he needs to check on another friend in another room to "
-               + "see why they have not come over yet. After he is gone for 10 minuets the power "
-               + "goes out. Now the fun begins.");
-       equationOne.setEquation ("Future Equation, insert here.");
-   
+               + "\nhosted by your friend Jack Clawsen, at his Dorm room. You eagerly accept"
+               + "\nhis invitation. You have not seen him since you were in grade school together."
+               + "\nYou make the hour long drive using the sirection provided to arrive at what "
+               + "\nappears to be a rundown set of dorms. You think, 'meh! Stayed in worse' and walk "
+               + "\ninto and up to his room. You knock and are greated by Jack with a hearty hello "
+               + "\nand a handshake. He asks how you have been and reminisces about the past. He  "
+               + "\nteases you about how much of a scardy cat you were. After an hour passes he "
+               + "\nexcuses himself and says he needs to check on another friend in another room to "
+               + "\nsee why they have not come over yet. After he is gone for 10 minuets the power "
+               + "\ngoes out. Now the fun begins.");
+       equationsOne.setEquations ("Future Equation, insert here.");
+       
        
        String playerInfo = playerOne.toString();
        String actorInfo = actorOne.toString();
-       String equationInfo = equationOne.toString();
+       String equationsInfo = equationsOne.toString();
+     
 
        
        System.out.println(playerInfo);
        System.out.println(actorInfo);
-       System.out.println(equationInfo);
+       System.out.println(equationsInfo);
+       
       
        
     }
