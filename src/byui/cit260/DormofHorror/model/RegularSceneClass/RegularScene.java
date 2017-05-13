3f67efb8d0f76f9d25a14d90fb9a5995c7e245c2
Office Payne <Office Payne@OfficePayne-PC>
2017-05-13 13:19:11 -0700

    5/13/2017 - Final revision for Daryl Payne

--- src/byui/cit260/DormofHorror/model/RegularSceneClass/RegularScene.java
+++ src/byui/cit260/DormofHorror/model/RegularSceneClass/RegularScene.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package byu.cit260.DormofHorror.model.RegularSceneClass;
+package byui.cit260.DormofHorror.model.RegularSceneClass;
 
 import java.io.Serializable;
 import java.util.Objects;
