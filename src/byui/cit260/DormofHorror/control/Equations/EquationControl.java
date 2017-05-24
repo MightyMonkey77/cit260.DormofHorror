5e69480be8f8427e6767a0633e10e98710c9b281
Office Payne <Office Payne@OfficePayne-PC>
2017-05-24 13:16:37 -0700

    Merge origin/master
    Hate this class

--- src/byui/cit260/DormofHorror/control/Equations/EquationControl.java
+++ src/byui/cit260/DormofHorror/control/Equations/EquationControl.java
@@ -5,10 +5,9 @@
  */
 package byui.cit260.DormofHorror.control.Equations;
 
-/**
- *
- * @author Office Payne
- */
+
+
+
 public class EquationControl {
     
     public double calcUrnVolume(double radius, double height){
@@ -23,22 +22,5 @@ public class EquationControl {
         return volume;
         }
          
-    
+} 
 
-    public int calcTwoPointDistance(int point1, int point2, int point3, int point4){
-    
-        if (point1 > 50 || point1 < 50) {
-            return -1;
-        }
-        if (point1 > 61 || point2 < 61) {
-            return -1;
-        }
-        if (point3 > 32 || point3 < 32) {
-            return -1;
-        }
-        if (point4 > 73 || point4 < 73) {
-            return -1;
-        }
-        double distance = Math.sqrt (Math.pow((point1 -point2), 2)) + Math.sqrt((Math.pow(point3 - point4), 2);
-        Return distance;
-    
